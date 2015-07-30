c9155809b2438f347d90fdde68e76a8323575fad
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-30 18:48:32 +0530

    generateTriplets Added

--- src/RummyHelper.java
+++ src/RummyHelper.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * Created by test on 7/30/2015.
  */
 public class RummyHelper {
+    public ArrayList<Integer[]> generateTriplets() {
+
+        ArrayList<Integer[]> tripletsList = new ArrayList<Integer[]>();
+
+        for (int i = 1; i <= 13; i++) {
+
+        }
+        Integer[] i = new Integer[3];
+        i[0] = 1;
+        i[1] = 14;
+        i[2] = 28;
+        tripletsList.add(i);
+        return tripletsList;
+    }
 }
