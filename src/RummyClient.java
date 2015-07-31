0c6964cabe1a986a05b60b0cf4766db8a592dd6f
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-31 11:10:57 +0530

    randomCardsGenerator function added

--- src/RummyClient.java
+++ src/RummyClient.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 /**
  * Created by test on 7/30/2015.
  */
 public class RummyClient {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int[] randomCardsGenerator() {
+        int[] cards = new int[13];
+        ArrayList<Integer> allCards = new ArrayList<Integer>();
+        for (int i = 1; i < 160; i++) {
+            allCards.add(new Integer(i));
+        }
+        Collections.shuffle(allCards);
+        for (int i = 0; i < 13; i++) {
+            cards[i] = allCards.get(i) % 53;
+        }
+
+        return cards;
+    }
+
 }
