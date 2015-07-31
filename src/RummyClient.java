ab4c48af17f03df13795ea77038020c6a2e1aa1b
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-31 13:59:59 +0530

    Rummy Client changed

--- src/RummyClient.java
+++ src/RummyClient.java
@@ -8,24 +8,10 @@ import java.util.HashMap;
 public class RummyClient {
 
     public static void main(String[] args) {
+        int length = 13;
         RummyHelper rummyHelper = new RummyHelper();
-        int[] cardsInHand = randomCardsGenerator();
+        int[] cardsInHand = Dealer.randomCardsGenerator(length);
         int minReplacements = rummyHelper.getMinimumReplacementsForWin(cardsInHand);
-        System.out.print("Minimum Replacements Required = "+minReplacements);
+        System.out.print("Minimum Replacements Required = " + minReplacements);
     }
-
-    static int[] randomCardsGenerator() {
-        int[] cards = new int[13];
-        ArrayList<Integer> allCards = new ArrayList<Integer>();
-        for (int i = 1; i < 160; i++) {
-            allCards.add(new Integer(i));
-        }
-        Collections.shuffle(allCards);
-        for (int i = 0; i < 13; i++) {
-            cards[i] = allCards.get(i) % 53;
-        }
-
-        return cards;
-    }
-
 }
